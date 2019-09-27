1c6754318aa9f87a9c9642db067544d1bca53d0b
Hanna Lee <mani21na@gmail.com>
2019-09-27 07:11:54 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  attr_accessible :name, :type, :db 
+  attr_reader :id 
+  
+  def initialize(id=nil, name, type, db)
+    @id = id
+    @name = name
+    @type = db
+  end
+  
+  def self.pokemon_from_db
+    
+  end 
 end
